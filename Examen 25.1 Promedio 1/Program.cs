fdc05b2146d354c90a6e9071b89d19642c371a01
Akinoid <130516420+Akinoid@users.noreply.github.com>
2025-04-23 14:00:23 -0500

    Creacion del Proyecto

---
+++ Examen 25.1 Promedio 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Examen_25._1_Promedio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
